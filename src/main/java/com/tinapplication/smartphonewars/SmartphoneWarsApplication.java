ea80b9bc36ce9d4306efddfc1e00dd63a0ca589f
Ti91 <julian-tinapp@hotmail.de>
2022-07-11 17:35:17 +0200

    Initial commit

---
+++ src/main/java/com/tinapplication/smartphonewars/SmartphoneWarsApplication.java
@@ -0,0 +1,13 @@
+package com.tinapplication.smartphonewars;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartphoneWarsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartphoneWarsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tinapplication/smartphonewars/SmartphoneWarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinapplication.smartphonewars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartphoneWarsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
