85589a34331a2c15eb5b59b0118026edd2beea27
Ti91 <julian-tinapp@hotmail.de>
2022-07-12 18:00:04 +0200

    added logger for application start

--- src/main/java/com/tinapplication/smartphonewars/SmartphoneWarsApplication.java
+++ src/main/java/com/tinapplication/smartphonewars/SmartphoneWarsApplication.java
@@ -1,13 +1,20 @@
 package com.tinapplication.smartphonewars;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SmartphoneWarsApplication {
+	private static final String TAG = "SmartphoneWars";
+
+	private static Logger logger = LoggerFactory.getLogger(SmartphoneWarsApplication.class);
 
 	public static void main(String[] args) {
-		SpringApplication.run(SmartphoneWarsApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SmartphoneWarsApplication.class, args);
+		logger.info(TAG, "Started");
 	}
-
 }
